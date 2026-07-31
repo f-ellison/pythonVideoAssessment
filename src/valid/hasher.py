e0e94c11e1156334fe39df7379a804a9797939d4
Frank Ellison <frankdellison@gmail.com>
2026-07-31 10:19:31 -0400

    Comment hasher.py for what the module needs to do.

---
+++ src/valid/hasher.py
@@ -0,0 +1,5 @@
+# Creates video signatures to find same content.
+
+# Grabbing sample frames from the video files.
+# Uses perceptual hashing to compare visual content.
+# Flags files as same content if hash distance is near zero.
